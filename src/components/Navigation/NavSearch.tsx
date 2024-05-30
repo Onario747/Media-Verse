cdc9952bb0a2cf71ee2b03ba3e7428102513318b
Onario747 <onarigeorge013@gmail.com>
2024-05-30 18:40:28 +0100

    "Added 'md:hidden' class to Search span element in NavSearch component"

--- src/components/Navigation/NavSearch.tsx
+++ src/components/Navigation/NavSearch.tsx
@@ -4,7 +4,7 @@ const NavSearch = () => {
   return (
     <div className="flex items-center gap-3">
       <IoIosSearch className="text-[2.1rem] text-white bg-red-600 rounded-full p-1 cursor-pointer" />
-      <span className="button-glow border-2 border-blue-500 rounded-full px-2 py-1">Search...</span>
+      <span className="button-glow border-2 border-blue-500 rounded-full px-2 py-1 md:hidden">Search...</span>
     </div>
   );
 }
